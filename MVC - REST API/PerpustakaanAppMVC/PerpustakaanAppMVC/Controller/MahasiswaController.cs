98315f091261f1b19f03458ccb8d416ca81e0819
hanoremma <emmahalimah049@gmail.com>
2026-01-02 09:25:47 +0700

    initial commit

---
+++ MVC - REST API/PerpustakaanAppMVC/PerpustakaanAppMVC/Controller/MahasiswaController.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+
+using PerpustakaanAppMVC.Model.Entity;
+using PerpustakaanAppMVC.Model.Repository;
+using PerpustakaanAppMVC.Model.Context;
+using System.Windows.Forms;
+
+namespace PerpustakaanAppMVC.Controller
+{
+    public class MahasiswaController
+    {
+        // deklarasi objek Repository untuk menjalankan operasi CRUD
+        private MahasiswaRepository _repository;
+
+        /// <summary>
+        /// Method untuk menampilkan data mahasiwa berdasarkan nama
+        /// </summary>
+        /// <param name="nama"></param>
+        /// <returns></returns>
+        public List<Mahasiswa> ReadByNama(string nama)
+        {
+            // membuat objek collection
+            List<Mahasiswa> list = new List<Mahasiswa>();
+
+            var repo = new MahasiswaRestApiRepository();
+            list = repo.ReadByNama(nama);
+
+            return list;
+        }
+
+        /// <summary>
+        /// Method untuk menampilkan semua data mahasiswa
+        /// </summary>
+        /// <returns></returns>
+        public List<Mahasiswa> ReadAll()
+        {
+            // membuat objek collection
+            List<Mahasiswa> list = new List<Mahasiswa>();
+
+            var repo = new MahasiswaRestApiRepository();
+            list = repo.ReadAll();
+
+            return list;
+        }
+
+        public int Create(Mahasiswa mhs)
+        {
+            int result = 0;
+
+            // cek npm yang diinputkan tidak boleh kosong
+            if (string.IsNullOrEmpty(mhs.Npm))
+            {
+                throw new ArgumentException("NPM harus diisi !!!");
+            }
+
+            // cek nama yang diinputkan tidak boleh kosong
+            if (string.IsNullOrEmpty(mhs.Nama))
+            {
+                throw new ArgumentException("Nama harus diisi !!!");
+            }
+
+            // cek angkatan yang diinputkan tidak boleh kosong
+            if (string.IsNullOrEmpty(mhs.Angkatan))
+            {
+                throw new ArgumentException("Angkatan harus diisi !!!");
+            }
+
+            var repo = new MahasiswaRestApiRepository();
+            result = repo.Create(mhs);
+
+            if (result > 0)
+            {
+                MessageBox.Show("Data mahasiswa berhasil disimpan !", "Informasi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Data mahasiswa gagal disimpan !", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            // membuat objek context menggunakan blok using
+
+            return result;
+        }
+
+        public int Update(Mahasiswa mhs)
+        {
+            int result = 0;
+
+            // cek npm yang diinputkan tidak boleh kosong
+            if (string.IsNullOrEmpty(mhs.Npm))
+            {
+                throw new ArgumentException("NPM harus diisi !!!");
+            }
+
+            // cek nama yang diinputkan tidak boleh kosong
+            if (string.IsNullOrEmpty(mhs.Nama))
+            {
+                throw new ArgumentException("Nama harus diisi !!!");
+            }
+
+            // cek angkatan yang diinputkan tidak boleh kosong
+            if (string.IsNullOrEmpty(mhs.Angkatan))
+            {
+                throw new ArgumentException("Angkatan harus diisi !!!");
+            }
+
+            var repo = new MahasiswaRestApiRepository();
+            result = repo.Update(mhs);
+
+            if (result > 0)
+            {
+                MessageBox.Show("Data mahasiswa berhasil diupdate !", "Informasi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Data mahasiswa gagal diupdate !", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            // membuat objek context menggunakan blok using
+
+            return result;
+        }
+
+        public int Delete(Mahasiswa mhs)
+        {
+            int result = 0;
+
+            // cek nilai npm yang diinputkan tidak boleh kosong
+            if (string.IsNullOrEmpty(mhs.Npm))
+            {
+                throw new ArgumentException("NPM harus diisi !!!");
+            }
+
+            var repo = new MahasiswaRestApiRepository();
+            result = repo.Delete(mhs.Npm);
+
+            if (result > 0)
+            {
+                MessageBox.Show("Data mahasiswa berhasil dihapus !", "Informasi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Data mahasiswa gagal dihapus !", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ MVC - REST API/PerpustakaanAppMVC/PerpustakaanAppMVC/Model/Repository/MahasiswaRestApiRepository.cs
@@ -0,0 +1,90 @@
+﻿using PerpustakaanAppMVC.Model.Entity;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerpustakaanAppMVC.Model.Repository
+{
+    public class MahasiswaRestApiRepository
+    {
+        public int Create(Mahasiswa mhs)
+        {
+            string baseUrl = "http://latihan.coding4ever.net:5555/";
+            var endpoint = "api/mahasiswa";
+
+            var client = new RestClient(baseUrl);
+            var request = new RestRequest(endpoint, Method.POST);
+            request.AddJsonBody(mhs);
+            var response = client.Execute(request);
+            var result = response.Content.IndexOf("1") > 0 ? 1 : 0;
+
+            return result;
+        }
+        public int Update(Mahasiswa mhs)
+        {
+            string baseUrl = "http://latihan.coding4ever.net:5555/";
+            var endpoint = "api/mahasiswa/NIM";
+
+            var client = new RestClient(baseUrl);
+            var request = new RestRequest(endpoint, Method.POST);
+            request.AddJsonBody(mhs);
+
+            var response = client.Execute(request);
+            var result = response.Content.IndexOf("1") > 0 ? 1 : 0;
+
+            return result;
+        }
+        public int Delete(string npm)
+        {
+            string baseUrl = "http://latihan.coding4ever.net:5555/";
+            var endpoint = "api/mahasiswa/NIM";
+
+            var client = new RestClient(baseUrl);
+            var request = new RestRequest(endpoint + "/" + npm, Method.DELETE);
+
+            var response = client.Execute(request);
+            var result = response.Content.IndexOf("1") > 0 ? 1 : 0;
+
+            return result;
+        }
+        public List<Mahasiswa> ReadAll()
+        {
+            string baseUrl = "http://latihan.coding4ever.net:5555/";
+            string endpoint = "api/mahasiswa";
+
+            var client = new RestClient(baseUrl);
+            var request = new RestRequest(endpoint, Method.GET);
+            var response = client.Execute<List<Mahasiswa>>(request);
+
+            return response.Data;
+        }
+        public List<Mahasiswa> ReadByNama(string nama)
+        {
+            string baseUrl = "http://latihan.coding4ever.net:5555/";
+            string endpoint = "api/mahasiswa";
+
+            var client = new RestClient(baseUrl);
+            var request = new RestRequest(endpoint, Method.GET);
+
+            var response = client.Execute<List<Mahasiswa>>(request);
+
+            return response.Data.FindAll(m => m.Nama.ToLower().Contains(nama.ToLower()));
+        }
+        public Mahasiswa ReadByNpm(string npm)
+        {
+            string baseUrl = "http://latihan.coding4ever.net:5555/";
+            string endpoint = "api/mahasiswa";
+
+            var client = new RestClient(baseUrl);
+            var request = new RestRequest(endpoint, Method.GET);
+
+            var response = client.Execute<List<Mahasiswa>>(request);
+
+            return response.Data.Find(m => m.Npm == npm);
+        }
+    }
+}
